9674a3bdce0254f24130ef0265d70a1d97be8f3b
Orr Shalev <shalev.orr@gmail.com>
2022-01-07 17:31:41 -0500

    Docs

--- src/pages/grafstate/Text.tsx
+++ src/pages/grafstate/Text.tsx
@@ -1,24 +1,36 @@
 import React from "react";
+import Navbar from "../../components/MathNavbar";
 
 interface Props {}
 
 interface State {
   num: number;
+  name: string;
 }
 
 class Text extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { num: 3 };
+    this.state = { num: 10, name: "Jimmy" };
     this.getNum();
+    this.getName();
   }
 
   getNum(): number {
     return this.state.num;
   }
 
+  getName(): string {
+    return this.state.name;
+  }
+
   render(): JSX.Element {
-    return <>{this.state.num}</>;
+    return (
+      <>
+        <div>{this.state.num}</div>
+        <div>{this.state.name}</div>
+      </>
+    );
   }
 }
 
